f19e4d41e1b41fa74bd80db98237f466f86a5e68
Johana Castellano <johanacg16@gmail.com>
2025-02-12 12:44:03 +0000

    iteration 1

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
-
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
@@ -12,10 +13,17 @@ function AllBeersPage() {
 
 
   // TASKS:
-  // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
-  // 2. Use axios to make a HTTP request.
-  // 3. Use the response data from the Beers API to update the state variable.
+  
+  const getAllBeers = () => {
+    axios // 2. Use axios to make a HTTP request.
+      .get(`${API_URL}`)
+      .then(response => setBeers(response.data)) // 3. Use the response data from the Beers API to update the state variable.
+      .catch((error) => console.log(error));
+  };
 
+  useEffect(() => {
+    getAllBeers(); // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
+  }, []);
 
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
@@ -27,7 +35,7 @@ function AllBeersPage() {
         {beers &&
           beers.map((beer, i) => {
             return (
-              <div key={i}>
+              <div key={beer._id}>
                 <Link to={"/beers/" + beer._id}>
                   <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
                     <div className="card-body">
